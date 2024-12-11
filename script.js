17c0d13f33c328598e535452050dccbdcabdad14
Joao Fabrício Moya Alves <140463692+JoaoFabricioMoyaAlves@users.noreply.github.com>
2024-12-10 23:06:09 -0300

    active
    active

---
+++ script.js
@@ -0,0 +1,24 @@
+
+$(function(){
+    $('.calc-btn').on('click',function(){
+        $('.result').val( $('.result').val()+ $(this).html());
+    })
+
+    $('.igual').on('click',function(){
+        exp =  $('.result').val() ;
+        if(exp.includes('x')){
+            exp = exp.replace('x','*');
+        }
+        $('.result').val( eval(exp) );
+    })
+
+    $('.clear').on('click',function(){
+        $('.result').val('');
+    })
+
+    $('.back').on('click',function(){
+       ex = $('.result').val();
+       $('.result').val(ex.substring(0,ex.length - 1));
+    })
+
+})
